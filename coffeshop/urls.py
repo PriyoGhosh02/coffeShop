9d09051c55f85717e1b7f2d6021e3e387dd7346a
PriyoGhosh02 <priyoghosh02@gmail.com>
2025-02-22 02:40:31 +0600

    add login,cart,menu basic template

--- coffeshop/urls.py
+++ coffeshop/urls.py
@@ -14,9 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
 from django.contrib import admin
 from django.urls import path
+from shop.views import home, menu, cart, CustomLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('menu/', menu, name='menu'),  # Add this line
+     path('cart/', cart, name='cart'),  # Cart Page
+    path('login/', CustomLoginView.as_view(), name='login'),  # Login Page
 ]
+
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+def home(request):
+    return render(request, 'index.html')
 
-# Create your views here.
+def menu(request):
+    return render(request, 'menu.html')  # Create this template
+
+
+def cart(request):
+    return render(request, 'cart.html')
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'  # Custom login template
